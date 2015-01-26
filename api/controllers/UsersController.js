c0284a3715206bfec20984f73ecfa7edf8d9db4d
ejongg <ejcampos08@gmail.com>
2015-01-26 21:06:06 +0800

    Delete unnecessary files

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -12,21 +12,21 @@ module.exports = {
 		Users.findOneByUsername(req.body.username)
 			.exec(function(err, user){
 				if(err)
-					return res.json({error: 'DB error'}, 500);			
+					return res.json({message: 'DB error'}, 500);			
 
 				if(user){
 					bcrypt.compare(req.body.password, user.password, function(err, match){
 						if(err)
-							return res.json({error : 'Server error'}, 500);
+							return res.json({message : 'Server error'}, 500);
 
 						if(match){
 							req.session.users = user.id;
 							return res.json(user);
 						}else
-							return res.json({error : 'Password do not match'}, 400);
+							return res.json({message : 'Password do not match'}, 400);
 					});
 				}else{
-					return res.json({error: 'User not found'}, 404);
+					return res.json({message: 'User not found'}, 404);
 				}
 			});
 	}
