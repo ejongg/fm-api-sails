afd6d0da11d50e26957172fc0a5a770984326d2d
ejongg <ejcampos08@gmail.com>
2015-02-02 20:30:22 +0800

    Minor changes in authentication

--- api/policies/tokenAuth.js
+++ api/policies/tokenAuth.js
@@ -1,9 +1,25 @@
 module.exports = function(req, res, next){
-	var token = req.headers.token;
+	var token;
+
+	if(req.headers.authorization){
+		var parts = req.headers.authorization.split(' ');
+
+		if(parts.length == 2){
+			if(parts[0] === 'Bearer'){
+				token = parts[1];
+			}
+		}else{
+			return res.json({error : 'Format is Authorization : Bearer [token]'});
+		}
+	}else{
+		return res.json({error : 'Authorization header not found'});
+	}
+	
+	
 
 	Auth.verifyToken(token, function(err, token){
 		if(err)
-			return res.json('Token not valid.');
+			return res.json({error : 'Token not valid.'});
 
 		req.token = token;
 
