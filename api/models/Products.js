21277f42c35bdeb8e4fae0ed382c1620e0319ce5
ejongg <ejcampos08@gmail.com>
2015-02-11 15:13:42 +0800

    Add web sockets support for Products model

--- api/models/Products.js
+++ api/models/Products.js
@@ -12,7 +12,7 @@ module.exports = {
   		type : 'string',
   		required : true
   	},
-  	brand : {
+  	company : {
   		type : 'string',
   		required : true
   	},
@@ -20,6 +20,14 @@ module.exports = {
       collection : 'product_details',
       via : 'prod_id'
     }
+  },
+
+  afterCreate : function(product, next){
+    Products.publishCreate({prod_name : product.prod_name, company : product.company, variants : product.variants});
+  },
+
+  afterUpdate = function(product, next){
+    Products.publishUpdate(product.id, product);
   }
 };
 
