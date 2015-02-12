73022f41e9065ac4b68096a2167a0c7ddec88c14
ejongg <ejcampos08@gmail.com>
2015-02-12 20:56:53 +0800

    Auth now working with sockets

--- api/policies/tokenAuth.js
+++ api/policies/tokenAuth.js
@@ -1,26 +1,34 @@
 module.exports = function(req, res, next){
 	var token;
+	
+	if(req.isSocket){
+		var auth = req.socket.handshake.headers.authorization;
 
-	if(req.headers.authorization){
-		var parts = req.headers.authorization.split(' ');
+		if(auth){
+			var parts = auth.split(' ');
 
-		if(parts.length == 2){
-			if(parts[0] === 'Bearer'){
-				token = parts[1];
+			if(parts.length == 2){
+				if(parts[0] === 'Bearer'){
+					token = parts[1];
+				}
+			}else{
+				return res.json({error : 'Wrong format'});
 			}
 		}else{
-			return res.json({error : 'Format is Authorization : Bearer [token]'});
+			return res.json({error : 'Authorization header not found'});
 		}
+		
+		Auth.verifyToken(token, function(err, token){
+			if(err)
+				return res.json({error : 'Token not valid.'});
+
+			req.token = token;
+
+			next();
+		});
+
 	}else{
-		return res.json({error : 'Authorization header not found'});
-	}
-	
-	Auth.verifyToken(token, function(err, token){
-		if(err)
-			return res.json({error : 'Token not valid.'});
-
-		req.token = token;
-
+		res.json({error : 'Request must be socket.'});
 		next();
-	});
+	}
 }
