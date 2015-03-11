5652ca6ff807de73797e1762b757abdf6456aa82
ejongg <ejcampos08@gmail.com>
2015-03-11 17:57:19 +0800

    Added Bad order module
    This module is not yet complete. It doesn't deduct the items in the
    inventory yet

--- api/controllers/Bad_ordersController.js
+++ api/controllers/Bad_ordersController.js
@@ -4,8 +4,39 @@
  * @description :: Server-side logic for managing bad_orders
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
+var moment = require('moment');
 
 module.exports = {
-	
+	add : function(req, res){
+		var products = req.body.products;
+
+		var bad_order = {
+			expense : req.body.total_expense,
+			date : moment().format('MM-DD-YYYY')
+		};
+
+		Bad_orders.create(bad_order)
+			.exec(function(err, created_bad_order){
+				_(products).forEach(function(product){
+					var item = {
+						bad_order_id : 	created_bad_order.id,
+						sku_id : product.sku_id,
+						cases : product.cases,
+						reason : product.reason
+					};
+
+					Bad_order_details.create(item)
+						.exec(function(err, created_bad_order_item){
+							/**
+							*	TODO : Deduct the number of cases that was
+							*	considered as bad order in the inventory
+							*
+							*	Question: How to determine what spefic item
+							*	in the inventory was labeled as bad order
+							*/
+						});
+				}).value();
+			});
+	}
 };
 
