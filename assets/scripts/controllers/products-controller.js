554117f713c0c4d21e934ffc9b7178c2dabb7c60
Adonique Pineda <devadonique@gmail.com>
2015-03-13 17:56:02 +0800

    Change $http to io.socket.request

--- assets/scripts/controllers/products-controller.js
+++ assets/scripts/controllers/products-controller.js
@@ -2,218 +2,155 @@
 
 angular.module('fmApp')
 .controller('ProductsCtrl',['$scope','$sailsSocket','_','$filter', function($scope, $sailsSocket, _,$filter){
-  $scope.addProduct = false;
-  $scope.newProductTab = true;
+
   $scope.products = [];
-  $scope.prod_rev = false;
-  $scope.company_rev = false;
-  
-  $scope.editOrDeleteProductPane = false;
-  $scope.editProductTab = true;
-  $scope.editOrDeleteProduct = {};
-
-  $scope.noExistingProduct = false;
-
-  $scope.product = {};
-  $scope.product.prod_name = '';
-  $scope.product.company = '';
-
-  $scope.existingCompanyProduct = {};
-  $scope.existingCompanyProduct.prod_name = '';
-  $scope.existingCompanyProduct.company = '';
-
   $scope.existingCompany = [];
 
-  $scope.errorMessage = "";
-  $scope.hasError = false;
+  $scope.product = {};
+  $scope.productExistingCompany = {};
+  $scope.productEdit = {};
+  $scope.productDelete = {};
+  $scope.copiedProduct = {};
 
-  $scope.pageSize = 5;
-  $scope.maxSize = 5;
-  $scope.totalItems = 0;
-  $scope.currentPage = 1;
+  $scope.addProductForm = false;
+  $scope.editOrDeleteProductForm = false;
+  $scope.newProductTab = true;
+  $scope.editProductTab = true;
 
-   var orderBy = $filter('orderBy');
 
-  $scope.showAddProduct = function (data) {
-    $scope.addProduct = data;
-    clearForm();
-    console.log($scope.editOrDeleteProductPane);
-    if($scope.editOrDeleteProductPane === true){
-      $scope.editOrDeleteProductPane = false;
-    }
-    if(data === false) {
-      if($scope.newProductTab === false){
-        $scope.setNewProductTab(true);
+  var getProducts = function () {
+    io.socket.request($scope.socketOptions('get','/products'), function (body, JWR) {
+      console.log('Sails responded with get user: ', body);
+      console.log('and with status code: ', JWR.statusCode);
+      if(JWR.statusCode === 200){
+        $scope.products = body;
+        $scope.existingCompany = _.uniq($scope.products, 'company');
+        $scope.productExistingCompany.company = $scope.existingCompany[0].company;
+        $scope.$digest();
       }
-      if($scope.hasError === true){
-        $scope.hasError = false;
-      }
-    }
+    });
   };
 
-  $scope.setNewProductTab = function (data) {
-    clearForm();
-    console.log($scope.noExistingProduct);
+  getProducts();
+
+  $scope.showAddProductForm = function (data) {
+      $scope.addProductForm = data;
+      if($scope.editOrDeleteProductForm === true) {
+        $scope.editOrDeleteProductForm(false);
+      }
+      if(data === false){
+        if($scope.newProductTab === false){
+          $scope.setNewProductTab(true);
+        }else{
+          $scope.product = {};
+        }
+      }
+  };
+
+  $scope.showEditOrDeleteProductForm = function (data) {
+      $scope.editOrDeleteProductForm = data;
+  };
+
+  $scope.setNewProductTab = function (data){
     $scope.newProductTab = data;
-  };
-
-  $scope.productClicked = function (product) {
-    $scope.editOrDeleteProduct = angular.copy(product);
-    if($scope.addProduct === true){
-      $scope.addProduct = false;
-    }
-    $scope.showEditOrDeleteProducts(true);
-  };
-
-  $scope.showEditOrDeleteProducts = function (data) {
-    $scope.editOrDeleteProductPane = data;
-    if(data === false) {
-      $scope.editOrDeleteProduct = {};
-      if($scope.editProductTab === false){
-        $scope.setEditProductTab(true);
-      }
+    if(data === true){
+      $scope.product = {};
+    }else{
+      $scope.productExistingCompany.prod_name = '';
+      $scope.productExistingCompany.company = $scope.existingCompany[0].company;
     }
   };
 
   $scope.setEditProductTab = function (data) {
     $scope.editProductTab = data;
+    if(data === true){
+      $scope.productEdit.id = $scope.copiedProduct.id;
+      $scope.productEdit.prod_name = $scope.copiedProduct.prod_name;
+      $scope.productEdit.company= $scope.copiedProduct.company;
+    } 
+  };
+    
+  $scope.productClicked = function (product) {
+    if($scope.addProductForm === true){
+      $scope.showAddProductForm(false);
+    }
+    
+    $scope.copiedProduct = angular.copy(product);
+
+    $scope.productEdit.id = $scope.copiedProduct.id;
+    $scope.productEdit.prod_name = $scope.copiedProduct.prod_name;
+    $scope.productEdit.company= $scope.copiedProduct.company;
+
+    $scope.productDelete.id = $scope.copiedProduct.id;
+    $scope.productDelete.prod_name = $scope.copiedProduct.prod_name;
+    $scope.productDelete.company= $scope.copiedProduct.company;
+
+    $scope.showEditOrDeleteProductForm(true);
   };
 
-  var clearForm =function () {
-    $scope.product.prod_name= "";
-    $scope.product.company="";
-    $scope.existingCompanyProduct.prod_name= "";
-    if($scope.noExistingProduct === false){
-      $scope.existingCompanyProduct.company = $scope.existingCompany[0].company;
-    }
-  };
-
-  var getProducts = function (){  
-    $sailsSocket.get('/products').success(function (data) {
-    $scope.products = data;
-    console.log($scope.products.length);  
-    if($scope.products.length === 0) {
-      $scope.noExistingProduct = true;
-    }else{
-      $scope.totalItems = $scope.products.length;
-      console.log($scope.products);
-      $scope.existingCompany = _.uniq($scope.products,'company');
-      console.log($scope.existingCompany);
-      // $scope.existingCompanyProduct.company = $scope.existingCompany[0].company;
-    }
-    }).error(function (err) {
-      console.log(err);
-    });
-  };
-  
-  getProducts();
-
-  $scope.addNewProduct = function(data){
-     var productInfo = {};
-    if(angular.isObject(data.company)){
-      var companyObject = data.company;
-      productInfo = {
-        prod_name: data.prod_name,
-        company: companyObject.company
-      };  
-    }else {
-     productInfo = {
-        prod_name: data.prod_name,
-        company: data.company
-     };
-
-    }
-
-    $sailsSocket.post('/products', productInfo).success(function (data) {
-      console.log(data);
-
-      if(_.findIndex($scope.existingCompany, function(prod) { return prod.company == data.company; }) === -1){
-        $scope.existingCompany.push(data);
-      }
-
-      $scope.products.push(data);
-
-      if($scope.noExistingProduct === true){
-        $scope.noExistingProduct = false;
-      }
-
-      $scope.totalItems = $scope.products.length;
-      console.log($scope.totalItems);
-      $scope.addProduct = false;
-      clearForm();
-      $scope.setNewProductTab(true);
-    }).error(function (err) {
-      if(err.error === 'E_VALIDATION'){
-        setFormError("Name is already used.");
-        clearForm();
+  $scope.addProduct = function (product) {
+    io.socket.request($scope.socketOptions('post','/products',{},product), function (body, JWR) {
+      console.log('Sails responded with post product: ', body);
+      console.log('and with status code: ', JWR.statusCode);
+      if(JWR.statusCode === 400){
+        console.log("User already exist");
       }
     });
-    // $scope.addProduct = false;
-    // clearForm();
-    // $scope.setNewProductTab(true);
   };
 
-  var setFormError = function (message) {
-    $scope.errorMessage = message;
-    $scope.showError(true);
-  }
-
-  $scope.showError = function (data) {
-    if(data === true) {
-      $scope.hasError = true;
-    }else{
-      $scope.hasError = false;
-    }
-  }
-
-  $scope.editProduct = function (data) {
-    var newInfo = {
-      "prod_name": data.prod_name,
-      "company": data.company
-    }
-    $sailsSocket.put('/products/' + data.id, newInfo).success(function (data) {
-      console.log(data.id);
-      var index = _.findIndex($scope.products, function(prod) { return prod.id == data.id; });
-      console.log(index);
-
-      if(_.findIndex($scope.existingCompany, function(prod) { return prod.company == data.company; }) === -1){
-        $scope.existingCompany.push(data);
+  $scope.editProduct = function (newInfo) {
+    io.socket.request($scope.socketOptions('put','/products/' + newInfo.id,{},newInfo), function (body, JWR) {
+      console.log('Sails responded with edit product: ', body);
+      console.log('and with status code: ', JWR.statusCode);
+      if(JWR.statusCode === 200){
+        
       }
-
-      $scope.products[index] = data;
-      $scope.existingCompany = _.uniq($scope.products,'company');
-    }).error(function (err) {
-      console.log(err);
     });
-
-    $scope.editOrDeleteProductPane = false;
   };
 
   $scope.deleteProduct = function (product) {
-    console.log(product.id);
-    $sailsSocket.delete('/products/' + product.id).success(function (data) {
-      console.log(data.id);
-    //   console.log("delete");
-
-      var index = _.findIndex($scope.products, function(prod) { return prod.id == data.id; });
-      console.log(index);
-      $scope.products.splice(index,1);
-      
-      if($scope.products.length === 0){
-        $scope.noExistingProduct = true;
+    console.log(product);
+    io.socket.request($scope.socketOptions('delete','/products/' + product.id,{}), function (body, JWR) {
+      console.log('Sails responded with delete product: ', body);
+      console.log('and with status code: ', JWR.statusCode);
+      if(JWR.statusCode === 200){
+        
       }
-      $scope.totalItems = $scope.products.length;
-      $scope.existingCompany = _.uniq($scope.products,'company');
-    }).error(function (err) {
-      console.log(err);
     });
-
-    $scope.editOrDeleteProductPane = false;
-    $scope.setEditProductTab(true);
   };
 
-  $scope.order = function(predicate, reverse) {
-    $scope.products = orderBy($scope.products, predicate, reverse);
-  };
+  io.socket.on('products', function(msg){
+    console.log("Message Verb: " + msg.verb);
+    console.log("Message Data :");
+    console.log(msg.data);
+    
+    switch (msg.verb) {
+      case "created": 
+        console.log("Product Created");
+        $scope.products.push(msg.data);
+        $scope.existingCompany = _.uniq($scope.products, 'company');
+        $scope.showAddProductForm(false);
+        $scope.$digest();
+        break;
+      case "updated":
+        console.log("Product Updated");
+        var index = _.findIndex($scope.products,{'id': msg.data.id});
+        $scope.products[index] = msg.data;
+        $scope.existingCompany = _.uniq($scope.products, 'company');
+        $scope.showEditOrDeleteProductForm(false);
+        $scope.$digest();
+        break;
+      case "destroyed":
+        console.log("Product Deleted");
+        var index = _.findIndex($scope.products,{'id': msg.data.id});
+        $scope.products.splice(index,1);
+        $scope.showEditOrDeleteProductForm(false);
+        $scope.$digest();
+    }
+  });
 
 }]);
+
+
+
+ 
--- assets/scripts/controllers/trucks-controller.js
+++ assets/scripts/controllers/trucks-controller.js
@@ -3,22 +3,31 @@
 angular.module('fmApp')
 .controller('TrucksCtrl',['$scope','$sailsSocket','_','$http', function($scope, $sailsSocket, _, $http){
   $scope.trucks = [];
-  $scope.editIndex = [];
+  $scope.editIndex = -1;
   $scope.truck = {};
   $scope.truckEdit = {};
 
   $scope.addTruckForm = false;
 
   var getTrucks = function () {
-      io.socket.get('/trucks');
-      $http.get('http://localhost:1337/trucks').success(function (data) {
-      $scope.trucks = data;
-      console.log("Trucks");
-      console.log($scope.trucks);
-      }).error(function (err) {
-      console.log("Trucks Error");
-      console.log(err);
-      });
+      // io.socket.get('/trucks');
+      // $http.get('http://localhost:1337/trucks').success(function (data) {
+      // $scope.trucks = data;
+      // console.log("Trucks");
+      // console.log($scope.trucks);
+      // }).error(function (err) {
+      // console.log("Trucks Error");
+      // console.log(err);
+      // });
+
+    io.socket.request($scope.socketOptions('get','/trucks'), function (body, JWR) {
+      console.log('Sails responded with get trucks: ', body);
+      console.log('and with status code: ', JWR.statusCode);
+      if(JWR.statusCode === 200){
+        $scope.trucks = body;
+        $scope.$digest();
+      }
+    });
   };
 
   getTrucks();
@@ -45,46 +54,95 @@ angular.module('fmApp')
   };
 
   $scope.addTruck = function (truck) {
-      console.log("Add Truck");
-      console.log(truck);
-      io.socket.post('http://localhost:1337/trucks', truck);
+      // console.log("Add Truck");
+      // console.log(truck);
+      // io.socket.post('http://localhost:1337/trucks', truck);
+      io.socket.request($scope.socketOptions('post','/trucks',{},truck), function (body, JWR) {
+      console.log('Sails responded with post truck: ', body);
+      console.log('and with status code: ', JWR.statusCode);
+      if(JWR.statusCode === 201){
+      
+      }
+    });  
   }; 
 
   $scope.editTruck = function (newInfo) {
-      console.log("Edit Truck");
-      console.log(newInfo);
-      io.socket.put('/trucks/' + newInfo.id, newInfo);
-      $scope.truckEditClicked(-1);
+      // console.log("Edit Truck");
+      // console.log(newInfo);
+      // io.socket.put('/trucks/' + newInfo.id, newInfo);
+      // $scope.truckEditClicked(-1);
+    io.socket.request($scope.socketOptions('put','/trucks/' + newInfo.id,{},newInfo), function (body, JWR) {
+      console.log('Sails responded with edit truck: ', body);
+      console.log('and with status code: ', JWR.statusCode);
+      if(JWR.statusCode === 200){
+        
+      }
+    });
   };
 
   $scope.deleteTruck = function (truckId) {
-      io.socket.delete('/trucks/' + truckId);
+      // io.socket.delete('/trucks/' + truckId);
+    io.socket.request($scope.socketOptions('delete','/trucks/' + truckId,{}), function (body, JWR) {
+      console.log('Sails responded with delete user: ', body);
+      console.log('and with status code: ', JWR.statusCode);
+      if(JWR.statusCode === 200){
+        
+      }
+    }); 
   };
 
-   io.socket.on('trucks', function(msg){
-            switch (msg.verb){
-                case 'created' :
-                     console.log("Truck Created");
-                    $scope.trucks.push(msg.data);
-                    $scope.$digest();
-                    break;
+   // io.socket.on('trucks', function(msg){
+   //          switch (msg.verb){
+   //              case 'created' :
+   //                   console.log("Truck Created");
+   //                  $scope.trucks.push(msg.data);
+   //                  $scope.$digest();
+   //                  break;
                     
-                case 'updated' :
-                    console.log("Truck Updated");
-                    var updated = msg.data;
-                    var index = _.findIndex($scope.trucks, {id : updated.id});
-                    
-                    $scope.trucks[index] = msg.data; 
-                    $scope.$digest();
-                    break;
-                case 'destroyed' : 
-                    console.log("Truck Destroyed");
-                    console.log(msg);
-                    var deleted = msg.data;
-                    var index = _.findIndex($scope.trucks, {id : deleted.id});
-                    $scope.trucks.splice(index,1);
-                    $scope.$digest();
-            }
-        });
+   //              case 'updated' :
+   //                  console.log("Truck Updated");
+   //                  var updated = msg.data;
+   //                  var index = _.findIndex($scope.trucks, {id : msg.data.id});
+   //                  $scope.trucks[index] = msg.data; 
+   //                  $scope.$digest();
+   //                  break;
+   //              case 'destroyed' : 
+   //                  console.log("Truck Destroyed");
+   //                  console.log(msg);
+   //                  var deleted = msg.data;
+   //                  var index = _.findIndex($scope.trucks, {id : deleted.id});
+   //                  $scope.trucks.splice(index,1);
+   //                  $scope.$digest();
+   //          }
+   //      });
+
+  io.socket.on('trucks', function(msg){
+    console.log("Message Verb: " + msg.verb);
+    console.log("Message Data :");
+    console.log(msg.data);
+
+    switch (msg.verb) {
+      case "created": 
+        console.log("Truck Created");
+        $scope.trucks.push(msg.data);
+        $scope.showAddTruckForm(false);
+        $scope.$digest();
+        break;
+      case "updated": 
+        console.log("Truck Updated");
+        var index = _.findIndex($scope.trucks, {id : msg.data.id});
+        $scope.trucks[index] = msg.data;
+        $scope.editIndex = -1; 
+        $scope.$digest();
+        break;
+      case "destroyed":
+        console.log("Truck Deleted");
+        var index = _.findIndex($scope.trucks, {id : msg.data.id});
+        $scope.trucks.splice(index,1);
+        $scope.editIndex = -1;
+        $scope.$digest();
+    }
+
+  });
 
 }]);
