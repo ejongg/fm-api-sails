1a6cfa617452474adab780159b069193d0fdfe22
ejongg <ejcampos08@gmail.com>
2015-03-13 22:28:21 +0800

    Added a function
    The function counts the product on each bay and returns it as an array.

--- api/controllers/BaysController.js
+++ api/controllers/BaysController.js
@@ -4,8 +4,42 @@
  * @description :: Server-side logic for managing bays
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
+var async = require('async');
 
 module.exports = {
-	
+	bayitems : function getBayProducts(req, res){
+		var bays_with_products = [];
+
+		async.series([
+			function(cb){
+				Bays.find().exec(function(err, bays){
+					(bays).forEach(function(bay){
+
+						Inventory.find({bay_id : bay.id})
+							.exec(function(err, products){
+								var total_count = 0;
+
+								async.each(products, function(product, cb){
+									total_count = total_count + product.physical_count;
+									cb();
+								}, function(err){
+									if(err) 
+										console.log(err);
+
+									bays_with_products.push({bay_id : bay.id, total_products : total_count});
+									cb();
+								});	
+							});	
+					});
+				});
+
+			},
+
+			function(cb){
+				return res.send(bays_with_products);
+				cb();
+			}
+		]);
+	}
 };
 
