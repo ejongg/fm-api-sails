faffa4950338bdd9648b15f0cd2abdb1556dd7d6
ejongg <ejcampos08@gmail.com>
2015-03-17 21:12:31 +0800

    Added new function
    Access the new function thru sku/available. This returns every sku in
    inventory that has bottles or physical cases.

--- api/controllers/SkuController.js
+++ api/controllers/SkuController.js
@@ -6,6 +6,24 @@
  */
 
 module.exports = {
-	
+	available : function(req, res){
+		var available = [];
+
+		Inventory.find()
+			.then(function(inventory_items){
+
+				(inventory_items).forEach(function(item){
+					if(item.physical_count > 0 || item.bottles > 0){
+						available.push(item);
+					}
+				});
+
+				return res.send(available);
+			},
+
+			function(err){
+				console.log(err);	
+			});
+	}
 };
 
