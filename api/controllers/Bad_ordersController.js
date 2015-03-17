0cd9e21dfb622dcc7793eed85865ee10ec8c2a32
ejongg <ejcampos08@gmail.com>
2015-03-17 09:05:21 +0800

    Now uses Inventory Service
    I've separated the deduction in inventory

--- api/controllers/Bad_ordersController.js
+++ api/controllers/Bad_ordersController.js
@@ -18,7 +18,7 @@ module.exports = {
 
 		Bad_orders.create(bad_order)
 			.exec(function(err, created_bad_order){
-				_(products).forEach(function(product){
+				(products).forEach(function(product){
 					var item = {
 						bad_order_id : 	created_bad_order.id,
 						sku_id : product.sku_id,
@@ -27,40 +27,24 @@ module.exports = {
 					};
 
 					Bad_order_details.create(item)
-						.exec(function(err, created_bad_order_item){
-							/**
-							*	TODO : Deduct the number of cases that was
-							*	considered as bad order in the inventory
-							*
-							*	Question: How to determine what spefic item
-							*	in the inventory was labeled as bad order
-							*/
-							async.series([
-								function deductInInventory(cb){
-									if(typeof product.bay_id == 'number'){
-										Inventory.findOne({sku_id : product.sku_id, bay_id : product.bay_id})
-											.exec(function(err, found_sku){
-												found_sku.bottles = found_sku.bottles - (product.cases * product.bottlespercase);
-												found_sku.physical_count = found_sku.physical_count - product.cases;
-												found_sku.logical_count = found_sku.logical_count - product.cases;
-												found_sku.save(function(err, saved){});
-											});
-										cb();	
-									}else{
-										cb();
-									}
-								},
-
-								function emit(cb){
-									sails.sockets.blast('bad_orders', created_bad_order);
-									return res.send(201);
-									cb();
-								}
-							]);
-							
-							
+						.exec(function(err, created_item){
+							if(err) 
+								console.log(err);
 						});
-				}).value();
+
+					async.series([
+						function deductInInventory(cb){
+							InventoryService.updateInventory(product.sku_id, product.bottles, product.cases, product.bottlespercase);
+							cb();	
+						},
+
+						function emit(cb){
+							sails.sockets.blast('bad_orders', created_bad_order);
+							return res.send(201);
+							cb();
+						}
+					]);
+				});
 			});
 	}
 };
