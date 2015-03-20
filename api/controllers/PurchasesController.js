e13ad9e67a8e7e5c5dcebb9eaa03d27da068856a
ejongg <ejcampos08@gmail.com>
2015-03-20 19:36:25 +0800

    Made the code easier to read

--- api/controllers/PurchasesController.js
+++ api/controllers/PurchasesController.js
@@ -12,25 +12,20 @@ var async = require('async');
 module.exports = {
 	add : function(req, res){
 		var products = req.body.products;
+		var totalCost = req.body.total_cost;
+		var user = req.body.user;
 
 		var purchase = {
-			total_cost : req.body.total_cost,
-			user : req.body.user,
+			total_cost : totalCost,
+			user : user,
 			date_received : moment().format('MM-DD-YYYY')
 		};
 		
-		/**
-		*	Create the purchase record
-		*/
 		Purchases.create(purchase)
-			.exec(function(err, newPurchase){
+			.then(function(newPurchase){
 
-				/**
-				*	For each of the products in the products array,
-				*	add a purchase_id attribute to each of the objects
-				*	then create the purchase_product.
-				*/
 				(products).forEach(function(prod){
+					
 					var productToBeCreated = {
 						purchase_id : newPurchase.id,
 						sku_id : prod.sku_id,
@@ -43,67 +38,41 @@ module.exports = {
 					Purchase_products.create(productToBeCreated)
 						.exec(function(err, newProduct){
 							if(err)
-								console.log(err);
+								console.log(err);														
+						});
 
-							/**
-							*	NOTE: The exp_date attr here is just temporary
-							* 	will change it to the proper one when the
-							*	lifespan of products is already known.
-							*
-							*	After creating the purchase and purchase_products
-							*	instances, add the products to the inventory.
-							*/
-							var item = {
-								bay_id : prod.bay_id, 
-								sku_id : prod.sku_id, 
-								exp_date : prod.prod_date, 
-								physical_count : prod.cases, 
-								logical_count : prod.cases
-							};
+					var bottlespercase = 0;
 
-
-							async.series([
-								function getTotalBottles(cb){
-									Sku.findOne({id : prod.sku_id})
-										.exec(function(err, found_sku){
-											item.bottles = found_sku.bottlespercase * prod.cases;
-										});
+					async.series([
+						function getBottlesPerCase(cb){
+							Sku.findOne({id : prod.sku_id})
+								.exec(function(err, sku){
+									bottlespercase = sku.bottlespercase;
 
 									cb();
-								},
+								});						
+						},
 
-								function updateInventory(cb){
-									/**
-									*	Check if the product already exists in the
-									*	inventory. If yes then only update the 
-									*	logical count and the physical count.
-									*/
-									Inventory.findOne({sku_id : item.sku_id, exp_date : item.exp_date, bay_id : prod.bay_id})
-										.exec(function(err, found){
-											if(err)
-												console.log(err);
+						function updateInventory(cb){
+							InventoryService.put(prod.sku_id, prod.cases, bottlespercase, prod.bay_id, prod.prod_date);
 
-											if(found){
-												found.physical_count = found.physical_count + parseInt(prod.cases, 10);
-												found.logical_count = found.logical_count + parseInt(prod.cases, 10);
-												found.bottles = found.bottles + item.bottles;
-												found.save(function(err, saved){});
+							cb();
+						}
+					]);
 
-												cb();
-											}else{
-												Inventory.create(item)
-													.exec(function(err, newItem){});
-
-												cb();
-											}
-										});
-								}
-							]);
-						});
 				});
 
-				sails.sockets.blast('purchases', {verb : 'created', data : newPurchase});
-				return res.send(201);				
+				
+				return newPurchase;				
+			},
+
+			function(err){
+				console.log(err);
+			})
+
+			.then(function(purchase){
+				sails.sockets.blast('purchases', {verb : 'created', data : purchase});
+				return res.send(201);
 			});
 	}
 };
