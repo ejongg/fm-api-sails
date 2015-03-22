2bbb42c05cd35753c3efee06cc3844980e867a98
ejongg <ejcampos08@gmail.com>
2015-03-22 09:52:59 +0800

    Added new controllers

---
+++ api/controllers/Incomplete_casesController.js
@@ -0,0 +1,11 @@
+/**
+ * Incomplete_casesController
+ *
+ * @description :: Server-side logic for managing incomplete_cases
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/RoutesController.js
@@ -0,0 +1,11 @@
+/**
+ * RoutesController
+ *
+ * @description :: Server-side logic for managing routes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
