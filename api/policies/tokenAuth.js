5bfd28dde69a0b117d5a0144473ac8ce9bdd3b23
ejongg <ejcampos08@gmail.com>
2015-03-22 12:42:41 +0800

    Turned on token authorizaion

--- api/policies/tokenAuth.js
+++ api/policies/tokenAuth.js
@@ -1,34 +1,28 @@
 module.exports = function(req, res, next){
 	var token;
 	
-	if(req.isSocket){
-		var auth = req.headers.authorization;
+	var auth = req.headers.authorization;
 
-		if(auth){
-			var parts = auth.split(' ');
+	if(auth){
+		var parts = auth.split(' ');
 
-			if(parts.length == 2){
-				if(parts[0] === 'Bearer'){
-					token = parts[1];
-				}
-			}else{
-				return res.json({error : 'Wrong format'});
+		if(parts.length == 2){
+			if(parts[0] === 'Bearer'){
+				token = parts[1];
 			}
 		}else{
-			return res.json({error : 'Authorization header not found'});
+			return res.json({error : "Unauthorized", message : "Wrong format"});
 		}
-		
-		Auth.verifyToken(token, function(err, token){
-			if(err)
-				return res.json({error : 'Token not valid.'});
-
-			req.token = token;
-
-			next();
-		});
-
 	}else{
-		res.json({error : 'Request must be socket.'});
-		next();
+		return res.json({error : "Unauthorized", message : "Token not found"});
 	}
+	
+	Auth.verifyToken(token, function(err, token){
+		if(err)
+			return res.json({error : 'Token not valid.'});
+
+		req.token = token;
+
+		next();
+	});
 }
