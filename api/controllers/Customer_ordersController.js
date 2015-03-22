815ff72b5580c27295bcc39e3cbff18bb99607bb
ejongg <ejcampos08@gmail.com>
2015-03-22 09:55:17 +0800

    Code Cleanup in customer orders

--- api/controllers/Customer_ordersController.js
+++ api/controllers/Customer_ordersController.js
@@ -16,62 +16,40 @@ module.exports = {
 		var user = req.body.user;
 		var total_amount = req.body.total_amount;
 
-		/**
-		*	Find a customer if it doesn't exist create a new one
-		*/
-		Customers.findOrCreate({establishment_name : customer.establishment}, {
+		var customer = {
 			establishment_name : customer.establishment,
 			owner_name : customer.owner,
 			address : customer.address,
 			distance_rating : customer.distance
-		}).exec(function(err, new_customer){
+		};
+
+		Customers.findOrCreate({establishment_name : customer.establishment}, customer)
+			.exec(function(err, new_customer){
+
 			if(err)
 				res.json({error : 'An error has occured'});
 
-			/**
-			*	Create a new customer order for the customer
-			*
-			*	I did assume that the date_received should be 
-			*	the date and time when the record was created.
-			*	This can be subject to change depending on
-			*	the suggestion of the client.
-			*
-			*/
 			Customer_orders.create({
 				customer_id : new_customer.id,
 				cokeagent_name : cokeagent_name,
-				date_received : moment().format('MM-DD-YYYY'),
+				date_received : moment().format('YYYY-MM-DD'),
 				user  : user,
 				total_amount : total_amount
 			}).exec(function(err, new_cust_order){
 				if(err)
 					res.json({error : 'An error has occured'});
 
-				/**
-				*	Inject the order id of the products in the order
-				*	and remove unneccesary attributes.
-				*/
-
-				_(orders).forEach(function(order){
+				(orders).forEach(function(order){
 					order.order_id = new_cust_order.id;
 					delete order.sku;
 					delete order.$$hashKey;
-				}).value();
+				});
 
-				/**
-				*	Create records of the products in the order
-				*	and blast to subscribed sockets that a new order
-				*	is created.
-				*/
 				Customer_order_products.create(orders)
 					.exec(function(err, new_orders){
 						if(err)
 							res.json({error : 'An error has occured'});
 
-						/**
-						* 	Find the newly created customer order then populate
-						*	the customer id
-						*/
 						Customer_orders.findOne({id : new_cust_order.id}).populate('customer_id')
 							.exec(function(err, populated_cust_order){
 
