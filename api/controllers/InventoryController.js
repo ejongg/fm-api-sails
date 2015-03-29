8bd2e13f74f22ae7d7bcab7fba503e9ca8413a5a
ejongg <ejcampos08@gmail.com>
2015-03-29 20:15:00 +0800

    Send new attribs

--- api/controllers/InventoryController.js
+++ api/controllers/InventoryController.js
@@ -11,19 +11,22 @@ module.exports = {
 	getInventory : function(req, res){
 		var inventory = [];
 
-		Inventory.find()
+		Inventory.find().populate('bay_id')
 			.then(function(inventoryItems){
 				return inventoryItems;
 			})
 
 			.then(function(inventoryItems){				
 				async.each(inventoryItems, function(item, cb){
-					
+
 					Sku.findOne({id : item.sku_id}).populate('prod_id')
 						.then(function(foundSku){
 							item.company = foundSku.prod_id.company;
 							item.brand_name = foundSku.prod_id.brand_name;
-							
+							item.sku_name = foundSku.sku_name;
+							item.size = foundSku.size;
+
+							console.log(item);
 							inventory.push(item);
 							cb();
 						});
