3fa9c5faaa03dd1a1c793e1de763201c7812e337
ejongg <ejcampos08@gmail.com>
2015-03-29 12:35:49 +0800

    Duplicate entry will not be created

--- api/controllers/EmployeesController.js
+++ api/controllers/EmployeesController.js
@@ -6,6 +6,22 @@
  */
 
 module.exports = {
-	
+	add : function(req, res){
+		var employee = {
+		    emp_fname = req.body.emp_fname,
+			emp_lname = req.body.emp_lname,
+			position = req.body.position,
+			office = req.body.office
+		}
+
+		Employees.find(employee)
+			.then(function(found_employee){
+				if(found_employee){
+					return res.send("You are entering a duplicate entry");
+				}else{
+					Employees.create(employee).exec(function(err, created_employee){});
+				}
+			})
+	}
 };
 
