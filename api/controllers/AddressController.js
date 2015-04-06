eb32c2e708ac97a7f7ff55b8040cf5342ef794cc
ejongg <ejcampos08@gmail.com>
2015-04-06 19:25:29 +0800

    Added assign route function

--- api/controllers/AddressController.js
+++ api/controllers/AddressController.js
@@ -6,6 +6,27 @@
  */
 
 module.exports = {
-	
+	assign_route : function(req, res){
+		var route = req.body.route;
+		var addressList = req.body.address;
+		var updatedAddressList = [];
+
+		async.each(addressList, function(address, cb){
+			Address.update({id : address.id}, {route_id : route})
+				.then(function(updatedAddress){
+					updatedAddressList.push(updatedAddress);
+					cb();
+				},
+
+				function(err){
+					cb(err);
+				});
+		}, function(err){
+			if(err)
+				return res.send(err);
+
+			return res.send(updatedAddressList);
+		});
+	}
 };
 
