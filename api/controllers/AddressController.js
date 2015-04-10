58bde343ea77d75cc5bccbab198e2032e2aa0612
ejongg <ejcampos08@gmail.com>
2015-04-10 15:24:26 +0800

    Added new function

--- api/controllers/AddressController.js
+++ api/controllers/AddressController.js
@@ -4,8 +4,38 @@
  * @description :: Server-side logic for managing addresses
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
+ var Promise = require('bluebird');
 
 module.exports = {
+	remove : function(req, res){
+		var address = req.body.address;
+		var route = req.body.route;
 
+		Address.findOne({id : address})
+			.then(function (foundAddress){
+				return new Promise(function(resolve, reject){
+					foundAddress.route_id = '';
+					foundAddress.save(function(err, saved){
+						if(err) reject(err);
+
+						resolve(saved);
+					});
+				});				
+			})
+
+			.then(function (updatedAddress){
+				var obj = {
+					address : updatedAddress,
+					route : route
+				};
+
+				sails.sockets.blast('address', {verb : 'created', data : obj});
+				return res.send("Address removed from route", 200);
+			})
+
+			.catch(function(err){
+				return res.send(err);
+			});
+	}
 };
 
