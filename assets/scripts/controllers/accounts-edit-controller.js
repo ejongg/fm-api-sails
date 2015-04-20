e0e5f10df15ca5bf5e898054f97193145639d12d
Adonique Pineda <devadonique@gmail.com>
2015-04-20 15:45:27 +0800

    change password in edit accounts

--- assets/scripts/controllers/accounts-edit-controller.js
+++ assets/scripts/controllers/accounts-edit-controller.js
@@ -5,6 +5,7 @@ angular.module('fmApp')
   $scope.editForm = 0;
   $scope.user = {};
   $scope.userEdit = {};
+  $scope.password = {};
 
   var getUser = function () {
     $http.get('http://localhost:1337/users/' + userService.getUser().id ).success(function(data){
@@ -23,12 +24,33 @@ angular.module('fmApp')
     $scope.editForm = data;
     if (data === 0) {
       $scope.userEdit = angular.copy($scope.user);
+      if ($scope.password !== null) {
+        $scope.password = {};
+      }
     }
   };
 
   $scope.editUser = function (user) {
     console.log(user);
     io.socket.request($scope.socketOptions('put','/users/' + user.id,{"Authorization": "Bearer " + authService.getToken()} , user), function (body, JWR) {
+      console.log('Sails responded with edit user: ', body);
+      console.log('and with status code: ', JWR.statusCode);
+      if(JWR.statusCode === 200){
+        $scope.showUsernameForm(0);
+        $scope.$digest();
+      }
+    }); 
+  };
+
+  $scope.changePassword = function (password) {
+    var passwordInfo = {
+      "old_password" : password.old_password,
+      "new_password" : password.new_password,
+      "user_id" : $scope.user.id
+    };
+    console.log(passwordInfo);
+    io.socket.request($scope.socketOptions('post','/users/changepassword/' + passwordInfo.user_id,{"Authorization": "Bearer " + 
+      authService.getToken()} , passwordInfo), function (body, JWR) {
       console.log('Sails responded with edit user: ', body);
       console.log('and with status code: ', JWR.statusCode);
       if(JWR.statusCode === 200){
