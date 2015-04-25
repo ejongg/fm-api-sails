998b7108742378df7682d9dfd16147bea8808ed2
ejongg <ejcampos08@gmail.com>
2015-04-25 09:25:26 +0800

    Modified available function

--- api/controllers/SkuController.js
+++ api/controllers/SkuController.js
@@ -4,45 +4,34 @@
  * @description :: Server-side logic for managing sku
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
+var Promise = require("bluebird");
 
 module.exports = {
 	available : function(req, res){
 		var available = [];
 
 		Inventory.find().populate('sku_id')
-			.then(function getAvailableSkus(inventory_items){
+			.then(function getAvailableSkus(inventoryItems){
+				return inventoryItems; 				
+			})
 
-				async.each(inventory_items, function (item, cb){
+			.each(function (item){
+				return new Promise(function (resolve, reject){
 					if(item.physical_count > 0 ){
 						Sku.findOne({id : item.sku_id.id}).populate('prod_id')
 							.then(function (sku){
-								if(available.length == 0){							
-									available.push(sku);
-									cb();
-								}else{
-									for(var i = 0 ; i < available.length; i++){
-										if(available[i].id != sku.id){
-											available.push(sku);																				
-										}								
-									}
-									cb();	
-								}
+								available.push(sku);
+								resolve();
 							});												
 					}else{
-						cb();
+						resolve();
 					}
+				})
+			})
 
-				}, function (err){
-					if(err)
-						console.log(err);
-
-					return res.send(available);
-				});				
-			},
-
-			function(err){
-				console.log(err);	
-			});
+			.then(function (){
+				return res.send(available);
+			})
 	}
 };
 
