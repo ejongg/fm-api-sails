10f83789f4c1d3e68c41b1eb9854179e371d87a1
ejongg <ejcampos08@gmail.com>
2015-06-07 12:47:30 +0800

    Made the code neater in the policy

--- api/policies/SMBInventoryPolicy.js
+++ api/policies/SMBInventoryPolicy.js
@@ -4,66 +4,65 @@ module.exports = function(req, res, next){
 	var orders = req.body.orders;
 	var notAvailable = [];
 
-	new Promise(function (resolve, reject){
+	new Promise(function (resolve){
 		resolve(orders);
 	})
 	.each(function (order){
 		return new Promise(function (resolve, reject){
-			SkuService.getCompanyName(order.sku_id)
-				.then(function (company){
-					if(company == 'SMB'){
+			SkuService.getCompanyName(order.sku_id).then(function (company){
+				if(company == 'SMB'){
 
-						BaysService.findMovingPile(order.sku_id, function(err, bayResult){
-							if(typeof bayResult == 'number'){
+					BaysService.findMovingPile(order.sku_id, function(err, bayResult){
+						if(typeof bayResult == 'number'){
 
-								Inventory.find({sku_id : order.sku_id, bay_id : bayResult})
-									.then(function (foundSku){
+							Inventory.find({sku_id : order.sku_id, bay_id : bayResult})
+								.then(function (foundSku){
 
-										if(foundSku){
-											var totalCaseCount = 0;
+									if(foundSku){
+										var totalCaseCount = 0;
 
-											_(foundSku).forEach(function (sku){
-												totalCaseCount = totalCaseCount + sku.physical_count;
-											});
+										_(foundSku).forEach(function (sku){
+											totalCaseCount = totalCaseCount + sku.physical_count;
+										});
 
-											if(order.bottles > 0){
-												order.cases  = order.cases + 1;
-											}
-
-											if(order.cases > totalCaseCount){
-												SkuService.getSkuCompleteName(order.sku_id)
-													.then(function (skuCompleteName){
-														notAvailable.push(skuCompleteName);
-														resolve();
-													})
-												
-											}else{
-												resolve();
-											}
-
-										}else{
-											return res.send({message : order.sku_name + ' not found in current moving pile'}, 400);							
+										if(order.bottles > 0){
+											order.cases  = order.cases + 1;
 										}
 
-									});	
+										if(order.cases > totalCaseCount){
+											SkuService.getSkuCompleteName(order.sku_id)
+												.then(function (skuCompleteName){
+													notAvailable.push(skuCompleteName);
+													resolve();
+												})
+											
+										}else{
+											resolve();
+										}
 
-							}else{
-								return res.send({message : bayResult}, 400);
-							}
-						});
-						
-					}else{
-						resolve();
-					}
-				})
+									}else{
+										return res.send({message : order.sku_name + ' not found in current moving pile'}, 400);							
+									}
+
+								});	
+
+						}else{
+							return res.send({message : bayResult}, 400);
+						}
+					});
+					
+				}else{
+					resolve();
+				}
+			})
 		});
 	})
 
-		.then(function (){
-			if(notAvailable.length > 0){
-				return res.send({message : 'Insufficient stocks in current moving pile', data : notAvailable}, 400);
-			}else{
-				next();
-			}
-		})
+	.then(function (){
+		if(notAvailable.length > 0){
+			return res.send({message : 'Insufficient stocks in current moving pile', data : notAvailable}, 400);
+		}else{
+			next();
+		}
+	})
 };
