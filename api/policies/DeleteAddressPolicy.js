65751356abf285442472baf2de7c5d54483b7914
ejongg <ejcampos08@gmail.com>
2015-06-10 00:43:58 +0800

    Temporarily disable removing address in route

--- api/policies/DeleteAddressPolicy.js
+++ api/policies/DeleteAddressPolicy.js
@@ -2,11 +2,12 @@ var Promise = require('bluebird');
 
 module.exports = function(req, res, next){
 	var addressId = req.param('id');
+	console.log(req.body.route);
 
 	Address.findOne({id : addressId}).then(function (foundAddress){
 
-		if(foundAddress.route_id == null){
-			next();
+		if(foundAddress.coke_route == null || foundAddress.smb_route == null){
+			/*next();*/
 		}else{
 			Routes.findOne({id : foundAddress.route_id}).then(function (foundRoute){
 				return res.send({message :'Can\'t delete. Address is assigned to route ' + foundRoute.route_name, data : {route : foundAddress.route_id}}, 400);
