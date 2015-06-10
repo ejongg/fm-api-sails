f01afa5ff19c0c601db3dfcb7c8a1629e9db5595
ejongg <ejcampos08@gmail.com>
2015-06-10 10:30:35 +0800

    Can now add same address but different days

--- api/policies/UniqueAddressPolicy.js
+++ api/policies/UniqueAddressPolicy.js
@@ -2,12 +2,13 @@ var Promise = require('bluebird');
 
 module.exports = function(req ,res, next){
 	var addressName = req.body.address_name;
+	var days = req.body.days.toString();
 
 	if(addressName == undefined){
 		addressName = req.body.address.address_name;
 	}
 
-	Address.findOne({address_name : addressName}).then(function (foundAddress){
+	Address.findOne({address_name : addressName, days : days}).then(function (foundAddress){
 
 		if(foundAddress){
 			return res.send({message : "Address already exists"}, 400);
