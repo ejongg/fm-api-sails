ac85912e355bb35fd03e4a33a034966f5e19c300
ejongg <ejcampos08@gmail.com>
2015-06-10 10:48:15 +0800

    Fix for address uniqueness policy

--- api/policies/UniqueAddressPolicy.js
+++ api/policies/UniqueAddressPolicy.js
@@ -2,11 +2,14 @@ var Promise = require('bluebird');
 
 module.exports = function(req ,res, next){
 	var addressName = req.body.address_name;
-	var days = req.body.days.toString();
+	var days = req.body.days;
 
 	if(addressName == undefined){
 		addressName = req.body.address.address_name;
+		days = req.body.address.address_name;
 	}
+
+	days.toString();
 
 	Address.findOne({address_name : addressName, days : days}).then(function (foundAddress){
 
