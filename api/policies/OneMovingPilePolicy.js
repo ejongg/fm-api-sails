573fcfb2f0d583ea96c7f8a89bf11c800413d390
ejongg <ejcampos08@gmail.com>
2015-06-10 11:57:38 +0800

    Fix bug in moving pile policy

--- api/policies/OneMovingPilePolicy.js
+++ api/policies/OneMovingPilePolicy.js
@@ -3,13 +3,18 @@ var Promise = require('bluebird');
 module.exports = function (req, res, next){
 	var skuId = req.body.sku;
 	var status = req.body.pile_status;
+	var bayId = req.body.id;
 
 	if(status == "Moving pile"){
 		Bays.findOne({sku_id : skuId, pile_status : "Moving pile"})
 			.then(function (foundBay){
 
 				if(foundBay){
-					return res.send({message : "Only one moving pile per sku is allowed"}, 400);
+					if(foundBay.id == bayId){
+						next();
+					}else{
+						return res.send({message : "Only one moving pile per sku is allowed"}, 400);
+					}
 				}else{
 					next();
 				}
