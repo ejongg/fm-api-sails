77ab1b1fd6008d96a3308e17dc3362d29de0f516
Adonique Pineda <devadonique@gmail.com>
2015-06-12 22:05:30 +0800

    add /edit in trucks

--- assets/scripts/controllers/trucks-controller.js
+++ assets/scripts/controllers/trucks-controller.js
@@ -290,7 +290,7 @@ angular.module('fmApp')
       console.log(editInfo);
       console.log(truck.id);
 
-    io.socket.request($scope.socketOptions('put','/trucks/' + truck.id,{"Authorization": "Bearer " + authService.getToken()},editInfo), function (body, JWR) {
+    io.socket.request($scope.socketOptions('put','/trucks/edit' + truck.id,{"Authorization": "Bearer " + authService.getToken()},editInfo), function (body, JWR) {
       console.log('Sails responded with edit truck: ', body);
       console.log('and with status code: ', JWR.statusCode);
       if(JWR.statusCode === 200){
