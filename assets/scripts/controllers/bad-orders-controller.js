6108ff2d95746c7070c1e2a5ce9b667eab22433e
Eugenio Pulido Jr <pulido.gene@gmail.com>
2015-06-18 02:22:10 +0800

    bad orders. validation msgsss. max extra bottles

--- assets/scripts/controllers/bad-orders-controller.js
+++ assets/scripts/controllers/bad-orders-controller.js
@@ -37,6 +37,9 @@ angular.module('fmApp')
   $scope.newlyAdded = {};
   $scope.index = 0;
 
+  $scope.maxExtraBottles = 0;
+  $scope.min = 0; 
+
   var setPage = function(){
     $scope.sortCriteria = 'id';
 
@@ -72,6 +75,10 @@ angular.module('fmApp')
     });
   };
 
+  $scope.checkIfZero = function (){
+
+  };
+
 
   $scope.getBays = function (sku){
     console.log(sku);
@@ -91,6 +98,10 @@ angular.module('fmApp')
     }).error(function (err) {
       $scope.checkError(err);
     });
+  };
+
+  $scope.getMaxExtraBottles = function (sku){
+    $scope.maxExtraBottles = sku.bottlespercase - 1;
   };
 
   $scope.changeCompany = function () {
@@ -223,7 +234,12 @@ angular.module('fmApp')
     if($scope.itemExistingError === true){
       $scope.showItemExistingError(false);
     }
-
+    if(product.bottles == null){
+      product.bottles = 0;
+    }
+    if(product.cases == null){
+      product.cases = 0;
+    }
     var productInfo = {
       "sku_id" : product.sku.id,
       "bay_id" : product.bay.id,
